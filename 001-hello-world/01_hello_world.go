73c5a5f887199fc82fc8f4a964bac52dc302a4b5
ray-abel11 <r.abel-obeya@semicolon.africa>
2020-12-31 21:59:23 +0100

    update

--- 001-hello-world/01_hello_world.go
+++ 001-hello-world/01_hello_world.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("hello world ")
+	fmt.Println("hello world ")
+	foo()
+	for i := 0; i <= 100; i++ {
+		if i%2 == 0 {
+			fmt.Println("even numbers only", i)
+		}
+	}
+}
+func foo() {
+	fmt.Print("foo function was called")
 }
